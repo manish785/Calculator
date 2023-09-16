728dd86d94fdebc77c260bc7bd7159c119bc73f6
manish785 <kumarachilish1997@gmail.com>
2023-09-16 19:22:01 +0530

    refactored the code

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 
-let string = '';
+let string = "";
 let arr = Array.from(buttons);
 
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if(e.target.innerHTML == '='){
+arr.forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML === '='){
             string = eval(string);
             input.value = string;
         }
-        else if (e.target.innerHTML == 'AC'){
-            string = '';
+        else if (e.target.innerHTML === 'AC'){
+            string = "";
             input.value = string;
         }
-        else if (e.target.innerHTML == 'DEL'){
+        else if (e.target.innerHTML === 'DEL'){
             string = string.substring(0, string.length-1);
             input.value = string;
         }
@@ -24,4 +24,4 @@ arr.forEach(button => {
             input.value = string;
         }
     })
-})
+});
